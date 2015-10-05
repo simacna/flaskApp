333c61f2cc1b736341ce4bfdeb2ca2f990f2a101
sina sima <simacna@gmail.com>
2015-10-05 18:18:32 -0400

    added content to main

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def homepage():
-	return "Hi there, how ya doin?"
+	return render_template("main.html")
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True, port=8000, host='0.0.0.0')
