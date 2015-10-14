e0e1a140a87528ed230662a012529083897b2f34
sina sima <simacna@gmail.com>
2015-10-14 12:09:26 -0400

    creating dictionary with nested arrays and adding topic/subclasses to allow dynamic navigation

--- content_management.py
+++ content_management.py
@@ -1,3 +1,10 @@
 def content():
 	"""content() will hold the logic behind the custom built search bar """
-	TOPIC_DICT = {"Basics": [[]]}
+	TOPIC_DICT = {"Basics": [["Introduction to Python", "/introduction-to-python/"], 
+													 ["Print functions", "/python-print-function/"],
+													 ["Math basics with Python 3", "/math-basics-python-3/"
+													 ]],
+								"Web Dev": []} #Web dev is now the second topic
+								#Introduction to Python
+	#will be the title and the link to that is immediately after. Each inner array will be a specific sub section of the basics
+	#section
