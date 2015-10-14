9bd65cc2a5aa2f44d3e315ee15efe26aa332a12b
sina sima <simacna@gmail.com>
2015-10-14 12:32:38 -0400

    refracted code, added header to main using extends method

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from content_management import Content #import Content() class 
+
+TOPIC_DICT = Content() #create variable and set it as the Content() class with the returned list
 
 app = Flask(__name__)
 
@@ -8,10 +11,11 @@ def homepage():
 
 @app.route('/slashboard/') #whatever function is provided  below the route will appear, hence both /slashboard and
 #/dashboard will render the 'hi' page
+
 @app.route('/dashboard/')
 def dashboard():
 	# return render_template("main.html")
-	return render_template("dashboard.html")
+	return render_template("dashboard.html", TOPIC_DICT = TOPIC_DICT) #the first TOPIC_DICT is what will be referenced in the HTML
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8000, host='0.0.0.0')
--- content_management.py
+++ content_management.py
@@ -1,4 +1,4 @@
-def content():
+def Content():
 	"""content() will hold the logic behind the custom built search bar """
 	TOPIC_DICT = {"Basics": [["Introduction to Python", "/introduction-to-python/"], 
 													 ["Print functions", "/python-print-function/"],
@@ -8,3 +8,5 @@ def content():
 								#Introduction to Python
 	#will be the title and the link to that is immediately after. Each inner array will be a specific sub section of the basics
 	#section
+
+	return TOPIC_DICT
